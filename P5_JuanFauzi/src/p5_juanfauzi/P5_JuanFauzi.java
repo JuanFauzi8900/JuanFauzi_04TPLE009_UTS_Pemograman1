af93c7f30da1d4dacf256c2adc93b95f83edef4b
Juan Fauzi <juanfauzi393@gmail.com>
2026-05-01 16:14:12 +0700

    Add files via upload

---
+++ P5_JuanFauzi/src/p5_juanfauzi/P5_JuanFauzi.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package p5_juanfauzi;
+
+/**
+ *
+ * @author DELL
+ */
+public class P5_JuanFauzi {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
